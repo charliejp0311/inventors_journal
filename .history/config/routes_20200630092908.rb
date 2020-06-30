01a8b048f3482196ad65ce650bad64591c3414e8
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-06-30 10:07:04 -0500

    updated routes
    I put in the routes I think that I will need and nested them where I thought they should be nested as of now.

---
+++ .history/config/routes_20200630092908.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'inventions/new'
+  get 'inventions/show'
+  get 'inventions/index'
+  get 'users/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
