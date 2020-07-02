5d8528c6afe4e7f676f7d8471ac5d8ce8c8a6cf2
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-07-02 11:03:03 -0500

    added before validation on invention controller

--- .history/app/controllers/inventions_controller_20200702103855.rb
+++ .history/app/controllers/inventions_controller_20200702103855.rb
@@ -1,6 +1,4 @@
 class InventionsController < ApplicationController
-    before_action :authenticate_user
-    skip_before_action :authenticate_user, only: [:index, :show]
     def index
         @inventions = Invention.all
     end
