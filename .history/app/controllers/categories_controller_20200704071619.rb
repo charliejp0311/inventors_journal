fd312ad0f223bafa25a698fea8c2bb6df09ecc32
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-07-04 07:39:04 -0500

    worked on problem solution form working on problem controller to allow for the nested solution

--- .history/app/controllers/categories_controller_20200704071619.rb
+++ .history/app/controllers/categories_controller_20200704071619.rb
@@ -4,9 +4,7 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
-  def show
-    @category = set_category
-  end
+ 
   def create
     @category = Category.find_or_create_by(category_params)
     redirect_to categories_path
